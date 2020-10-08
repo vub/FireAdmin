36ec80d3738964f50676f9aaae2bee8416b94b15
Bui Minh Vu <Vu.Mihn.Bui@topicus.nl>
2020-10-08 18:42:38 +0700

    Update

--- projects/demo/src/environments/environment.ts
+++ projects/demo/src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDM1ybQFpUe3fJ046KAfJeoiBHl5M51Rt0",
-    authDomain: "fireadmin-74e50.firebaseapp.com",
-    databaseURL: "https://fireadmin-74e50.firebaseio.com",
-    projectId: "fireadmin-74e50",
-    storageBucket: "fireadmin-74e50.appspot.com",
-    messagingSenderId: "1081829121945",
-    appId: "1:1081829121945:web:349839b2c6846c71143d08"
+    apiKey: "AIzaSyC9g78ZuH7B3_45KVTUfr0-5rB11mZkfuA",
+    authDomain: "swan-9769e.firebaseapp.com",
+    databaseURL: "https://swan-9769e.firebaseio.com",
+    projectId: "swan-9769e",
+    storageBucket: "swan-9769e.appspot.com",
+    messagingSenderId: "614287691968",
+    appId: "1:614287691968:web:6a9461625d92db5a1b1d25",
+    measurementId: "G-00GHB9N6X7"
   }
 };
 
