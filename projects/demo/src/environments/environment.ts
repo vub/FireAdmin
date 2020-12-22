b63940fa0ac90a04be08c54ee2beefd4598672ea
Bui Minh Vu <Vu.Mihn.Bui@topicus.nl>
2020-12-22 19:54:59 +0700

    Update

--- projects/demo/src/environments/environment.ts
+++ projects/demo/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC9g78ZuH7B3_45KVTUfr0-5rB11mZkfuA",
-    authDomain: "swan-9769e.firebaseapp.com",
-    databaseURL: "https://swan-9769e.firebaseio.com",
-    projectId: "swan-9769e",
-    storageBucket: "swan-9769e.appspot.com",
-    messagingSenderId: "614287691968",
-    appId: "1:614287691968:web:6a9461625d92db5a1b1d25",
-    measurementId: "G-00GHB9N6X7"
+    apiKey: "AIzaSyAxIwL5hV5aob9Rwg32OEWpnP84_7lSy6Y",
+    authDomain: "littlenail.firebaseapp.com",
+    databaseURL: "https://littlenail.firebaseio.com",
+    projectId: "littlenail",
+    storageBucket: "littlenail.appspot.com",
+    messagingSenderId: "772341827966",
+    appId: "1:772341827966:web:283eacc44db10650708b2f",
+    measurementId: "G-RSWW0GX1GF"
   }
 };
 
